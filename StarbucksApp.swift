b48bd6c59a39f9eb25101829917addb1d5ea56f4
Nafeez Ahmed <119474829+NafSyed@users.noreply.github.com>
2023-11-06 14:51:09 +0530

    Just A Simple Starbucks Animation
    A simple starbucks animation created for learning purposes

---
+++ StarbucksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarbucksApp.swift
+//  Starbucks
+//
+//  Created by Nafeez Ahmed on 08/10/23.
+//
+
+import SwiftUI
+
+@main
+struct StarbucksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
